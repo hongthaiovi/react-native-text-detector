6ef6f210c758a4a93ba30166082bb873878d5340
Trần Hồng Thái <hongthaiovi@gmail.com>
2020-06-03 09:38:47 +0700

    Fixed crash android

--- android/src/main/java/com/fetchsky/RNTextDetector/RNTextDetectorModule.java
+++ android/src/main/java/com/fetchsky/RNTextDetector/RNTextDetectorModule.java
@@ -96,7 +96,7 @@ public class RNTextDetectorModule extends ReactContextBaseJavaModule {
 
             info.putMap("bounding", coordinates);
             info.putString("text", block.getText());
-            info.putDouble("confidence", block.getConfidence());
+//            info.putDouble("confidence", block.getConfidence());
             data.pushMap(info);
         }
 
